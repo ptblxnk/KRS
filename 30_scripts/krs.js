2a9f9ed8082855d6a6c7ffb343ce5f0e11d3744e
ptblxnk <30389498+ptblxnk@users.noreply.github.com>
2020-12-11 23:02:58 -0800

    V7 - Initial Commit
    - Background image now cycles through images
    - Revised Navbar font size
    -  Added underline when hovering in NAVBAR
    - Added social media icons to home page & SIGN UP/LOGIN FORMS
    - Cleaned krs.js
    - Archived V6 files

--- 30_scripts/krs.js
+++ 30_scripts/krs.js
@@ -1,7 +1,7 @@
-var btn = document.getElementById("sip");
-var btn2 = document.getElementById("sin");
-var x = document.querySelector(".popUp");
-var y = document.querySelector(".popIn");
+var  btn = document.getElementById("sip"),
+    btn2 = document.getElementById("sin"),
+       x = document.querySelector(".popUp"),
+       y = document.querySelector(".popIn");
 
 btn.addEventListener("click", function() {
   if (x.style.display === "flex") {
@@ -26,48 +26,18 @@ x.addEventListener("click", function() {
   }
 });
 
-//--REFACTOR WITH A "element.contains(event.target)..." approach--//
 y.addEventListener("click", function() {
   if(event.target.id != "info" && event.target.id != "info2" && event.target.id != "options" && event.target.id != "fp" && event.target.id != "login" && event.target.id != "welcome" && event.target.id != "forgot" && event.target.id != "submit") {
     y.style.display = "none";
   }
 });
+//--------------------------------------------------------------------------------------------------------//
 
+var pages = [feed, buy, sell, trade, about];
+var b = document.getElementById("navLinks");
 
-//-------------------THIS CAN BE REFACTORED AS WELL..MAYBE INTO AN ARRAY OR SOMETHING--------------------------------//
-
-var feed = document.getElementById("feed");
-var buy = document.getElementById("buy");
-var sell = document.getElementById("sell");
-var trade = document.getElementById("trade");
-var about = document.getElementById("about");
-
-feed.addEventListener("click", function() {
-  if(event.target.id === "feed") {
-    alert("This page is under construction.")
-  }
-})
-
-buy.addEventListener("click", function() {
-  if(event.target.id === "buy") {
-    alert("This page is under construction.")
-  }
-})
-
-sell.addEventListener("click", function() {
-  if(event.target.id === "sell") {
-    alert("This page is under construction.")
-  }
-})
-
-trade.addEventListener("click", function() {
-  if(event.target.id === "trade") {
-    alert("This page is under construction.")
-  }
-})
-
-about.addEventListener("click", function() {
-  if(event.target.id === "about") {
-    alert("This page is under construction.")
-  }
+b.addEventListener("click", function(){
+    if (event.target.id === "feed" || event.target.id === "buy" || event.target.id === "sell" || event.target.id === "trade" || event.target.id === "about") {
+        alert("The page " + event.target.id + " is under construction.")
+    } 
 })
